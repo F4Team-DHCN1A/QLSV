9f07971b8e368b21b28929b53290e4aaa4fd6c09
minhthuanit-21 <sktthuan111@gmail.com>
2017-06-29 11:14:16 +0700

    update

---
+++ QLSV_111/QuanLySinhVien_GUI/quanlydiem.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace QuanLySinhVien_GUI
+{
+    partial class quanlydiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(quanlydiem));
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDiemThi = new System.Windows.Forms.TextBox();
+            this.lblDiemThi = new System.Windows.Forms.Label();
+            this.txtDiemQuaTrinh = new System.Windows.Forms.TextBox();
+            this.lblDiemGK = new System.Windows.Forms.Label();
+            this.lblMaGV = new System.Windows.Forms.Label();
+            this.lblDiemKT = new System.Windows.Forms.Label();
+            this.lblMaMH = new System.Windows.Forms.Label();
+            this.lblDiemCC = new System.Windows.Forms.Label();
+            this.lblMaSV = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dgvKetQua = new System.Windows.Forms.DataGridView();
+            this.lblMaHK = new System.Windows.Forms.Label();
+            this.lblTittle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(203, 240);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 20);
+            this.textBox5.TabIndex = 54;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(203, 193);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 20);
+            this.textBox4.TabIndex = 53;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(203, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.TabIndex = 52;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(203, 149);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 51;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 367);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 48;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(353, 367);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 47;
+            // 
+            // txtDiemThi
+            // 
+            this.txtDiemThi.Location = new System.Drawing.Point(353, 291);
+            this.txtDiemThi.Name = "txtDiemThi";
+            this.txtDiemThi.Size = new System.Drawing.Size(60, 20);
+            this.txtDiemThi.TabIndex = 49;
+            // 
+            // lblDiemThi
+            // 
+            this.lblDiemThi.AutoSize = true;
+            this.lblDiemThi.Location = new System.Drawing.Point(29, 370);
+            this.lblDiemThi.Name = "lblDiemThi";
+            this.lblDiemThi.Size = new System.Drawing.Size(49, 13);
+            this.lblDiemThi.TabIndex = 41;
+            this.lblDiemThi.Text = "Điểm Thi";
+            // 
+            // txtDiemQuaTrinh
+            // 
+            this.txtDiemQuaTrinh.Location = new System.Drawing.Point(147, 291);
+            this.txtDiemQuaTrinh.Name = "txtDiemQuaTrinh";
+            this.txtDiemQuaTrinh.Size = new System.Drawing.Size(60, 20);
+            this.txtDiemQuaTrinh.TabIndex = 50;
+            // 
+            // lblDiemGK
+            // 
+            this.lblDiemGK.AutoSize = true;
+            this.lblDiemGK.Location = new System.Drawing.Point(235, 370);
+            this.lblDiemGK.Name = "lblDiemGK";
+            this.lblDiemGK.Size = new System.Drawing.Size(65, 13);
+            this.lblDiemGK.TabIndex = 42;
+            this.lblDiemGK.Text = "Điểm giữa kì";
+            // 
+            // lblMaGV
+            // 
+            this.lblMaGV.AutoSize = true;
+            this.lblMaGV.Location = new System.Drawing.Point(85, 243);
+            this.lblMaGV.Name = "lblMaGV";
+            this.lblMaGV.Size = new System.Drawing.Size(77, 13);
+            this.lblMaGV.TabIndex = 44;
+            this.lblMaGV.Text = "Mã Giảng Viên";
+            // 
+            // lblDiemKT
+            // 
+            this.lblDiemKT.AutoSize = true;
+            this.lblDiemKT.Location = new System.Drawing.Point(235, 294);
+            this.lblDiemKT.Name = "lblDiemKT";
+            this.lblDiemKT.Size = new System.Drawing.Size(71, 13);
+            this.lblDiemKT.TabIndex = 40;
+            this.lblDiemKT.Text = "Điểm kiểm tra";
+            // 
+            // lblMaMH
+            // 
+            this.lblMaMH.AutoSize = true;
+            this.lblMaMH.Location = new System.Drawing.Point(85, 196);
+            this.lblMaMH.Name = "lblMaMH";
+            this.lblMaMH.Size = new System.Drawing.Size(69, 13);
+            this.lblMaMH.TabIndex = 45;
+            this.lblMaMH.Text = "Mã Môn Học";
+            // 
+            // lblDiemCC
+            // 
+            this.lblDiemCC.AutoSize = true;
+            this.lblDiemCC.Location = new System.Drawing.Point(29, 294);
+            this.lblDiemCC.Name = "lblDiemCC";
+            this.lblDiemCC.Size = new System.Drawing.Size(90, 13);
+            this.lblDiemCC.TabIndex = 43;
+            this.lblDiemCC.Text = "Điểm chuyên cần";
+            // 
+            // lblMaSV
+            // 
+            this.lblMaSV.AutoSize = true;
+            this.lblMaSV.Location = new System.Drawing.Point(85, 149);
+            this.lblMaSV.Name = "lblMaSV";
+            this.lblMaSV.Size = new System.Drawing.Size(70, 13);
+            this.lblMaSV.TabIndex = 46;
+            this.lblMaSV.Text = "Mã Sinh Viên";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLuu.BackgroundImage")));
+            this.btnLuu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLuu.Location = new System.Drawing.Point(256, 412);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(72, 30);
+            this.btnLuu.TabIndex = 39;
+            this.btnLuu.Text = "      Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnThoat.BackgroundImage")));
+            this.btnThoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnThoat.Location = new System.Drawing.Point(135, 412);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(72, 30);
+            this.btnThoat.TabIndex = 38;
+            this.btnThoat.Text = "      Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // dgvKetQua
+            // 
+            this.dgvKetQua.AllowUserToAddRows = false;
+            this.dgvKetQua.AllowUserToDeleteRows = false;
+            this.dgvKetQua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKetQua.Location = new System.Drawing.Point(430, 37);
+            this.dgvKetQua.Name = "dgvKetQua";
+            this.dgvKetQua.ReadOnly = true;
+            this.dgvKetQua.Size = new System.Drawing.Size(361, 404);
+            this.dgvKetQua.TabIndex = 37;
+            // 
+            // lblMaHK
+            // 
+            this.lblMaHK.AutoSize = true;
+            this.lblMaHK.Location = new System.Drawing.Point(85, 99);
+            this.lblMaHK.Name = "lblMaHK";
+            this.lblMaHK.Size = new System.Drawing.Size(60, 13);
+            this.lblMaHK.TabIndex = 36;
+            this.lblMaHK.Text = "Mã Học Kỳ";
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTittle.ForeColor = System.Drawing.Color.Red;
+            this.lblTittle.Location = new System.Drawing.Point(67, 30);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(305, 31);
+            this.lblTittle.TabIndex = 35;
+            this.lblTittle.Text = "Chức Năng Quản Lý Điểm";
+            // 
+            // quanlydiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 503);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtDiemThi);
+            this.Controls.Add(this.lblDiemThi);
+            this.Controls.Add(this.txtDiemQuaTrinh);
+            this.Controls.Add(this.lblDiemGK);
+            this.Controls.Add(this.lblMaGV);
+            this.Controls.Add(this.lblDiemKT);
+            this.Controls.Add(this.lblMaMH);
+            this.Controls.Add(this.lblDiemCC);
+            this.Controls.Add(this.lblMaSV);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.dgvKetQua);
+            this.Controls.Add(this.lblMaHK);
+            this.Controls.Add(this.lblTittle);
+            this.Name = "quanlydiem";
+            this.Text = "QUẢN LÝ ĐIỂM";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDiemThi;
+        private System.Windows.Forms.Label lblDiemThi;
+        private System.Windows.Forms.TextBox txtDiemQuaTrinh;
+        private System.Windows.Forms.Label lblDiemGK;
+        private System.Windows.Forms.Label lblMaGV;
+        private System.Windows.Forms.Label lblDiemKT;
+        private System.Windows.Forms.Label lblMaMH;
+        private System.Windows.Forms.Label lblDiemCC;
+        private System.Windows.Forms.Label lblMaSV;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dgvKetQua;
+        private System.Windows.Forms.Label lblMaHK;
+        private System.Windows.Forms.Label lblTittle;
+    }
+}
