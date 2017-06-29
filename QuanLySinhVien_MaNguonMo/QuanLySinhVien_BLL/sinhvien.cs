a88c7219db738c5b298f305edfa2fd157ac92724
BoyBanhMi <boybanhmi86@gmail.com>
2017-06-29 11:13:28 +0700

    Update

---
+++ QuanLySinhVien_MaNguonMo/QuanLySinhVien_BLL/sinhvien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLySinhVien_BLL
+{
+    public class sinhvien
+    {
+        //public string strconn = "";
+       /* public static bool KiemTra_DN()
+        {
+            return true;
+        }*/
+        public static void Nhap_SV(string masv, string tensv, string ngaysinh, string malop)
+        {
+                SqlConnection con = new SqlConnection(QuanLySinhVien_DAL.DB_connect.strcon);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into SINHVIEN values('" + masv + "','" + tensv + "','" + ngaysinh + "','" + malop + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            
+        }
+        public static void Sua_SV(string masv, string tensv, string ngaysinh, string malop)
+        {
+            SqlConnection con = new SqlConnection(QuanLySinhVien_DAL.DB_connect.strcon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update from SINHVIEN set TenSV='"+tensv+"',NgaySinh='"+ngaysinh+"',MaLop='"+malop+"' where MaSV='"+masv+"'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void Xoa_SV(string masv)
+        {
+            SqlConnection con = new SqlConnection(QuanLySinhVien_DAL.DB_connect.strcon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from SINHVIEN  where MaSV='" + masv + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
---
+++ QuanLySinhVien_MaNguonMo/QuanLySinhVien_GUI/timkiemdiemsinhvientheoten.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace QuanLySinhVien_GUI
+{
+    partial class timkiemdiemsinhvientheoten
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(timkiemdiemsinhvientheoten));
+            this.cmbMaMH = new System.Windows.Forms.ComboBox();
+            this.lblMaMH = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dgvKetQua = new System.Windows.Forms.DataGridView();
+            this.lblMaSV = new System.Windows.Forms.Label();
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.btnTim = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbMaMH
+            // 
+            this.cmbMaMH.FormattingEnabled = true;
+            this.cmbMaMH.Location = new System.Drawing.Point(359, 77);
+            this.cmbMaMH.Name = "cmbMaMH";
+            this.cmbMaMH.Size = new System.Drawing.Size(121, 21);
+            this.cmbMaMH.TabIndex = 41;
+            // 
+            // lblMaMH
+            // 
+            this.lblMaMH.AutoSize = true;
+            this.lblMaMH.Location = new System.Drawing.Point(279, 79);
+            this.lblMaMH.Name = "lblMaMH";
+            this.lblMaMH.Size = new System.Drawing.Size(60, 13);
+            this.lblMaMH.TabIndex = 40;
+            this.lblMaMH.Text = "Mã Học Kỳ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 39;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnThoat.BackgroundImage")));
+            this.btnThoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnThoat.Location = new System.Drawing.Point(535, 385);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(72, 30);
+            this.btnThoat.TabIndex = 37;
+            this.btnThoat.Text = "      Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // dgvKetQua
+            // 
+            this.dgvKetQua.AllowUserToAddRows = false;
+            this.dgvKetQua.AllowUserToDeleteRows = false;
+            this.dgvKetQua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKetQua.Location = new System.Drawing.Point(12, 127);
+            this.dgvKetQua.Name = "dgvKetQua";
+            this.dgvKetQua.ReadOnly = true;
+            this.dgvKetQua.Size = new System.Drawing.Size(566, 190);
+            this.dgvKetQua.TabIndex = 36;
+            // 
+            // lblMaSV
+            // 
+            this.lblMaSV.AutoSize = true;
+            this.lblMaSV.Location = new System.Drawing.Point(14, 80);
+            this.lblMaSV.Name = "lblMaSV";
+            this.lblMaSV.Size = new System.Drawing.Size(74, 13);
+            this.lblMaSV.TabIndex = 35;
+            this.lblMaSV.Text = "Tên Sinh Viên";
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTittle.ForeColor = System.Drawing.Color.Red;
+            this.lblTittle.Location = new System.Drawing.Point(66, 16);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(414, 31);
+            this.lblTittle.TabIndex = 34;
+            this.lblTittle.Text = "Tìm Kiếm Điểm Theo Tên Sinh Viên";
+            // 
+            // btnTim
+            // 
+            this.btnTim.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTim.BackgroundImage")));
+            this.btnTim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnTim.Location = new System.Drawing.Point(520, 71);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(72, 30);
+            this.btnTim.TabIndex = 42;
+            this.btnTim.Text = "       Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            // 
+            // timkiemdiemsinhvientheoten
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 444);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.cmbMaMH);
+            this.Controls.Add(this.lblMaMH);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.dgvKetQua);
+            this.Controls.Add(this.lblMaSV);
+            this.Controls.Add(this.lblTittle);
+            this.Name = "timkiemdiemsinhvientheoten";
+            this.Text = "TÌM KIẾM ĐIỂM SINH VIÊN";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMaMH;
+        private System.Windows.Forms.Label lblMaMH;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dgvKetQua;
+        private System.Windows.Forms.Label lblMaSV;
+        private System.Windows.Forms.Label lblTittle;
+        private System.Windows.Forms.Button btnTim;
+    }
+}
